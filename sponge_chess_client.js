4f0fb404317a26946bde4f517728a55fde070fca
shufy <shufsp@gmail.com>
2023-10-05 18:31:05 -0500

    initial commit

---
+++ sponge_chess_client.js
@@ -0,0 +1,255 @@
+(() => {
+  // =========================================================
+  //   SPONGE CHESS CLIENT v2.0-beta
+  // 	 CHESSBOARD VERSION: 1.56.0
+  // =========================================================
+
+  const manageSocketResource = (board, socket) => {
+    const interval = setInterval(() => {
+      if (board.game.isGameOver()) {
+        clearInterval(interval);
+        socket.close() // terminate script
+      }
+    }, 1000);
+  }
+
+
+  const cheese = (board) => {
+    const originalBoardGameState = board.game;
+    const spongePrint = (msg) => console.log(`[SPONGE CHESS] ${msg}`);
+
+    // OUR SOCKET CONNECTION
+    spongePrint("Initializing socket ...")
+    spongePrint("CONNECTING TO SPONGECHESS SOCKET SERVER (YOU BETTER HAVE IT RUNNING)")
+    const socket = new WebSocket("ws://localhost:8765");
+    manageSocketResource(board, socket);
+
+    // OUR CHEAT
+    spongePrint("Initializing move markers ...")
+    const coordinates = document.getElementsByClassName("coordinates")[0];
+    const clock = document.getElementsByClassName("clock-time-monospace")[1]
+    const moveMarkerFrom = document.createElementNS("http://www.w3.org/2000/svg", "text");
+    moveMarkerFrom.setAttribute("font-size", "5");
+    moveMarkerFrom.textContent = "💦";
+    const moveMarkerTo = document.createElementNS("http://www.w3.org/2000/svg", "text");
+    moveMarkerTo.setAttribute("font-size", "5");
+    moveMarkerTo.textContent = "🧽";
+
+    coordinates.appendChild(moveMarkerFrom);
+    coordinates.appendChild(moveMarkerTo);
+
+    const coordinatesConfig = {
+      originWhite: {
+        x: 0.75,
+        y: 3.50
+      },
+      originBlack: {
+        x: 88.5,
+        y: 90.0
+      },
+      gapWhite: {
+        x: 12.50,
+        y: 12.25
+      },
+      gapBlack: {
+        x: -12.50,
+        y: -12.25
+      }
+    }
+    const getTimeLeft = () => {
+      if (!clock) {
+        return {
+          minutes: 99999,
+          seconds: 99999,
+          string: "No time limit"
+        }
+      }
+      const splitClock = clock.textContent.split(":")
+      const minutesLeft = splitClock[0];
+      const secondsLeft = clock.textContent.includes(".") ? splitClock[1].split(".")[0] : splitClock[1];
+      return {
+        minutes: parseInt(minutesLeft),
+        seconds: parseInt(secondsLeft),
+        string: clock.textContent
+      }
+    }
+    const getPiecesLeft = () => Object.keys(board.game.getPieces().getCollection()).length;
+    const getSquarePixelPosition = (square) => {
+      const playingAsWhite = board.game.getPlayingAs() == 1;
+
+      const squareOffsetX = parseInt(square[0], 36) - 10;
+      const squareOffsetY = 8 - parseInt(square[1]);
+
+      const originX = playingAsWhite ? coordinatesConfig.originWhite.x : coordinatesConfig.originBlack.x;
+      const originY = playingAsWhite ? coordinatesConfig.originWhite.y : coordinatesConfig.originBlack.y;
+
+      const gapX = playingAsWhite ? coordinatesConfig.gapWhite.x : coordinatesConfig.gapBlack.x;
+      const gapY = playingAsWhite ? coordinatesConfig.gapWhite.y : coordinatesConfig.gapBlack.y;
+
+      const pixelPositionX = originX + (squareOffsetX * gapX);
+      const pixelPositionY = originY + (squareOffsetY * gapY);
+      return {
+        x: pixelPositionX,
+        y: pixelPositionY
+      };
+    }
+    const displayMove = (move) => {
+      const fromPosition = getSquarePixelPosition(move.substring(0, 2))
+      const toPosition = getSquarePixelPosition(move.substring(2, 4))
+
+      moveMarkerFrom.setAttribute("x", fromPosition.x);
+      moveMarkerFrom.setAttribute("y", fromPosition.y);
+      moveMarkerTo.setAttribute("x", toPosition.x);
+      moveMarkerTo.setAttribute("y", toPosition.y);
+    };
+    const sendCommand = (async (command) => {
+      //spongePrint(`Sending socket command ${command}`)
+      await socket.send(command)
+    });
+
+    const originalMoveFunction = board.game.move;
+    const originalSelectNodeFunction = board.game.selectNode;
+
+    const handleResponseBestMoves = (async (response) => {
+      // TODO: only show one move for now
+      const moves = response.split(":")[1];
+      if (moves === "None") {
+        spongePrint("Game Ended. Exiting script :)")
+        return; // the game is over
+      }
+      //spongePrint("Displaying Move")
+      displayMove(moves)
+
+      // move automatically
+      const moveOptions = {
+        color: board.game.getPlayingAs(),
+        flags: 2,
+        from: moves.substring(0, 2),
+        to: moves.substring(2, 4),
+        lines: null,
+        animate: true,
+        userGenerated: true,
+        userGeneratedDrop: true,
+      }
+      const timeLeft = getTimeLeft();
+      const piecesCount = getPiecesLeft()
+      const minDelay = 500;
+      const maxDelay = (piecesCount * 40) + 650;
+
+      const randomDelay = Math.random() * (maxDelay - minDelay) + minDelay;
+      spongePrint(`Delay: ${randomDelay} (${minDelay} - ${maxDelay})`)
+
+      const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+      if (timeLeft.minutes === 0 && timeLeft.seconds < 10) {
+        // HURRY UP!
+        spongePrint("RUSH MODE! LESS THAN 10 SECONDS LEFT")
+        await delay(Math.random() * (450 - 120) + 120)
+      } else if (board.game.getHistoryFENs().length < 20) {
+        // book moves
+        spongePrint("Delay shortened for book moves")
+        await delay(randomDelay / 2)
+      } else await delay(randomDelay);
+      originalMoveFunction(moveOptions);
+    });
+    const handleResponseDefault = (response) => {
+      spongePrint(`Default response handler ran (${response})`)
+    }
+    const onMoveHook = (move) => {
+      //spongePrint(`get_best_moves:${move.fen}`);
+      sendCommand(`get_best_moves:${move.fen}`)
+    }
+
+
+    // CHEAT HOOKS
+    // Instead of creating mutation observers or infinite while loops,
+    // We intercept the functions to tell at the exact moment when a next move has been played
+    spongePrint("Initializing chessboard hooks ...")
+
+    board.game.move = (...args) => {
+      // occurs on an actual new move
+      //console.log("Moving ... ", ...args, args.length)
+      originalMoveFunction.apply(board.game, args);
+      //spongePrint(`FEN After Move: ${board.game.getFEN()}`)
+      onMoveHook({
+        fen: board.game.getFEN()
+      })
+    };
+    board.game.selectNode = (...args) => {
+      // occurs when moving forward or backward in move history
+      // hooking this so you can go back to previous moves and see the best move at that position
+      //console.log("SelectNode", ...args, args.length)
+      spongePrint("A node in the moves tree was selected")
+      originalSelectNodeFunction.apply(board.game, args)
+      onMoveHook({
+        fen: board.game.getFEN()
+      })
+    }
+
+
+    // SOCKET HANDLERS
+
+    socket.onopen = (event) => {
+      const ELO = 1500
+      const DEPTH = 5
+
+      spongePrint(`Connected to SpongeChess socket server: ${event.data}`);
+      sendCommand(`get_best_moves:${board.game.getFEN()}`);
+      sendCommand(`set_depth:${DEPTH}`)
+      sendCommand(`set_elo:${ELO}`)
+      spongePrint("")
+      spongePrint("====================================")
+      spongePrint(`ELO: ${ELO}`)
+      spongePrint(`DEPTH: ${DEPTH}`)
+      spongePrint("====================================")
+      spongePrint("")
+    }
+    socket.onmessage = (event) => {
+      const msg = event.data;
+      spongePrint(`Server ~ $ ${msg}`)
+      if (msg.startsWith("best_move")) {
+        handleResponseBestMoves(msg);
+      } else {
+        handleResponseDefault(msg);
+      }
+    };
+    socket.onclose = (event) => {
+      // remove markers
+      moveMarkerFrom.remove()
+      moveMarkerTo.remove()
+
+      // un-hook our cheat
+      board.game.move = originalMoveFunction;
+      board.game.selectNode = originalSelectNodeFunction;
+      board.game = originalBoardGameState; // just for good measure
+
+      spongePrint("Socket closing, un-hooking spongechess...")
+
+      if (event.wasClean) {
+        spongePrint(`Connection closed cleanly, code=${event.code}, reason=${event.reason}`);
+      } else {
+        spongePrint("Connection closed");
+      }
+    };
+  };
+
+  const waitUntilOurFirstTurn = (callback) => {
+    const waitInterval = setInterval(() => {
+      const ourClock = document.getElementsByClassName("clock-bottom")[0]
+      if (ourClock && ourClock.classList.contains("clock-player-turn")) {
+        // its now our turn
+        clearInterval(waitInterval)
+        callback();
+      }
+    })
+  }
+
+  (() => {
+    waitUntilOurFirstTurn(() => {
+      // For some reason, the ENTIRE game state is stored in this html tag 
+      const board = document.getElementsByClassName("board")[0];
+      cheese(board)
+    })
+  })();
+
+})()
---
+++ sponge_chess_server.py
@@ -0,0 +1,69 @@
+import asyncio
+import websockets
+from stockfish import Stockfish
+
+stockfish = Stockfish("/usr/bin/stockfish")
+
+# Engine cheat presets
+presets = {
+    "bullet": {"elo": 1800, "depth": 2},
+    "blitz": {"elo": 2200, "depth": 3},
+    "rapid": {"elo": 2600, "depth": 4},
+    "noob": {"elo": 800, "depth": 1},
+    "blatant": {"elo": 3000, "depth": 15}
+}
+
+# Applies a preset to stockfish engine
+def apply_preset(preset_str="blitz"):
+    preset = presets.get(preset_str)
+    if preset is None:
+        raise ValueError(f"Preset '{preset_str}' is not valid")
+    stockfish.set_elo_rating(preset["elo"])
+    stockfish.set_depth(preset["depth"])
+    print(f"Switched to preset '{preset_str}'")
+
+# Gets stats about the current board configuration
+def display_stats():
+    #print(stockfish.get_board_visual())
+    print()
+    print(f"Eval: {stockfish.get_evaluation()}")
+    print()
+    print(f"FEN: {stockfish.get_fen_position()}")
+    print()
+
+async def websocket_handler(websocket, path):
+    print("New client connection")
+    await websocket.send("WebSocket connection established")
+
+    try:
+        async for message in websocket:
+            # Handle incoming WebSocket messages here
+            if message.startswith("get_best_moves"):
+                _, fen = message.split(":")
+                stockfish.set_fen_position(fen.strip())
+                print(f"Best move asked for fen {fen}")
+                await websocket.send(f"best_move:{stockfish.get_best_move()}")
+            elif message.startswith("apply_preset"):
+                _, preset_str = message.split(":")
+                apply_preset(preset_str.strip())
+            elif message.startswith("set_elo"):
+                _, elo = message.split(":")
+                elo = int(elo)
+                stockfish.set_elo_rating(elo)
+                await websocket.send(f"elo:{elo}")
+            elif message.startswith("set_depth"):
+                _, depth = message.split(":")
+                depth = int(depth)
+                stockfish.set_depth(depth)
+                await websocket.send(f"depth:{depth}")
+    except websockets.exceptions.ConnectionClosed:
+        print("WebSocket connection closed")
+
+
+if __name__ == "__main__":
+    PORT = 8765
+    start_server = websockets.serve(websocket_handler, "0.0.0.0", PORT)
+    print(f"SpongeChess socket server running on port {PORT}")
+    print(f"get_best_moves, apply_preset:<preset>, set_elo:<ELO>, set_depth:<DEPTH>")
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
